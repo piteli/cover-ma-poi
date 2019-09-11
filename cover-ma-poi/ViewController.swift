89e39bea6b21419a76999c2a35ee8965a382dddf
Fitri San <ahmadfitrisan@gmail.com>
2019-09-11 16:16:52 +0800

    Initial Commit

---
+++ cover-ma-poi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cover-ma-poi
+//
+//  Created by Fitri San on 9/11/19.
+//  Copyright © 2019 com.piteli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
