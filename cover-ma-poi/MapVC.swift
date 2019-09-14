47a40103485e1139b7bcee2114977382bc93a108
piteli <rushwender@gmail.com>
2019-09-14 15:49:17 +0800

    added map interfaces, auto zoom, current user location and auto center location when click the button at the bottom

--- cover-ma-poi/MapVC.swift
+++ cover-ma-poi/MapVC.swift
@@ -8,22 +8,49 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius : Double = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
     }
 
     @IBAction func centerMapWerePressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse{
+            centerMapOnUserLocation()
+        }
     }
     
 }
 
 extension MapVC : MKMapViewDelegate {
-    
+    func centerMapOnUserLocation(){
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegion(center : coordinate, span : MKCoordinateSpan(latitudeDelta: regionRadius, longitudeDelta: regionRadius))
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 }
 
+extension MapVC : CLLocationManagerDelegate {
+    func configureLocationServices(){
+        if authorizationStatus == .notDetermined{
+            locationManager.requestAlwaysAuthorization()
+        }else{
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+}
